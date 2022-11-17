9f4c52b9c34e133ff1f2737c6874d44b3bfa80c5
DariaaaP <barinova_f@list.ru>
2022-11-17 16:46:32 +0400

    added project

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import SuperHero from './Superheroes_card.jsx';
+import datas from "./../data.json";
+import './../style/App.css';
+
+function App() {
+  return (
+    <div className="App"> {
+      datas.map((data) =>
+        <SuperHero name={data.name} univers={data.univers} alter_ego={data.alter_ego} career={data.career} friend={data.friend} superpower={data.superpower} img={data.img} />
+      )
+    }
+    </div>
+  )
+}
+export default App;
---
+++ src/components/Superheroes_card.jsx
@@ -0,0 +1,25 @@
+import './../style/index.css';
+
+function SuperHero(props) {
+    return (
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <h2>{props.name}</h2>
+                    <img src={props.img} alt="superhoroes_img" />
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <div><span className="pName">Вселенная:</span> {props.univers}</div>
+                    <div><span className="pName">Альтер эго:</span> {props.alter_ego}</div>
+                    <div><span className="pName">Род деятельности:</span> {props.career}</div>
+                    <div><span className="pName">Друзья:</span> {props.friend}</div>
+                    <div><span className="pName">Суперсилы:</span>{props.superpower}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SuperHero;
